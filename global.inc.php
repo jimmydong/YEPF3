2721d0c2379a344d86597cc6afbecb4f3ecf860b
yisheng <jimmy@yisheng>
2015-03-10 18:31:11 +0800

    support classname.lib.php

--- global.inc.php
+++ global.inc.php
@@ -81,8 +81,11 @@ class YEPFCore {
       		 */
 			$class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
         	$class_path = getCustomConstants('CUSTOM_CLASS_PATH') . DIRECTORY_SEPARATOR . $class_name.'.class.php';
+        	$class_path2 = getCustomConstants('CUSTOM_CLASS_PATH') . DIRECTORY_SEPARATOR . $class_name.'.lib.php';
         	if(file_exists($class_path)) {
         		return include_once($class_path);
+        	}elseif(file_exists($class_path2)){
+        		return include_once($class_path2);
         	}else{
         		//支援将驼峰转变为目录结构
         		$lastdspos = strripos($class_name, DIRECTORY_SEPARATOR);
