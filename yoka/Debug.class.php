53580feb0123f9e08f82c06f2be666cb92d161e2
jimmy <jimmy.dong@gmail.com>
2017-02-09 18:30:15 +0800

    upgrade

--- yoka/Debug.class.php
+++ yoka/Debug.class.php
@@ -508,6 +508,7 @@ class Debug
 					$data[] = $tmp;
 				}
 			}
+			if(count($data) == 0)continue;
 			if(self::$firephp == 'chrome')\ChromePhp::groupCollapsed($title);
 			\ChromePhp::table($data);
 			if(self::$firephp == 'chrome')\ChromePhp::groupEnd();
