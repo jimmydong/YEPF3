26fae3e5ebd56a506d6ce33d494c92afa83c00ab
jimmy <jimmy.dong@gmail.com>
2017-03-01 14:12:16 +0800

    dlog不重复记录

--- yoka/Debug.class.php
+++ yoka/Debug.class.php
@@ -853,6 +853,7 @@ class Debug
 				if($db){
 					if(self::$db_log_mysql)foreach (self::$db_table as $v){
 						if(! preg_match('/insert|update|delete/i',$v[3])) continue; //全部记录太大了
+						if(preg_match('/^INSERT INTO debug_log SET/i',$v[3])) continue; //dlog不做重复记录
 						$values[] = "('".self::get_real_ip()."','".$_SERVER["SERVER_ADDR"]."','db','','','".addslashes($_SERVER["REQUEST_URI"])."','" .addslashes($v[0]). ":" .addslashes($v[1]). "','" .addslashes($v[2]). "','" .addslashes($v[3]). "','" .addslashes(var_export($v[4], true)). "')";
 					}
 					if(self::$debug_log_mysql)foreach (self::$log_table as $v){
