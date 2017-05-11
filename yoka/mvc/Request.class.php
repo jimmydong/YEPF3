b6a4c0103f72c83b5f37033b5b8d1b351fa4daf8
jimmy <jimmy.dong@gmail.com>
2017-05-11 11:31:00 +0800

    update

--- yoka/mvc/Request.class.php
+++ yoka/mvc/Request.class.php
@@ -35,6 +35,8 @@ class Request {
 	}
 
 	public function post($index, $default='') {
+		if($index == '' || $default == '')
+			return $_POST;
 		return isset($_POST[$index])? $_POST[$index]:$default;
 	}
 	
