7f53a98b6940c54ae48e9a0b39a0fe0bd820e4e7
jimmy <jimmy.dong@gmail.com>
2017-07-10 17:31:32 +0800

    update

--- yoka/Debug.class.php
+++ yoka/Debug.class.php
@@ -358,7 +358,7 @@ class Debug
 			return ;
 		}
 		if(is_string($ip) && strlen($ip)>24)$ip = substr($ip, 0, 24) . '..';
-		if(is_string($sql) && strlen($sql)>96)$sql = substr($sql, 0, 96) . '..';
+		if(is_string($sql) && strlen($sql)>128)$sql = substr($sql, 0, 128) . '..';
 		if(is_string($results) && strlen($results)>256)$results = substr($results,0,256) . '...(length:'.strlen($results).')';
 		array_push(self::$db_table, array($ip, $database, $times, $sql, $results));
 	}
