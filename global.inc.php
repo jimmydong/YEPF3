c15857fd6aab428b3f3ec2a6a355febd2813c409
jimmy <jimmy.dong@gmail.com>
2017-09-14 15:55:55 +0800

    update

--- global.inc.php
+++ global.inc.php
@@ -109,23 +109,25 @@ class YEPFCore {
 }
 YEPFCore::registerAutoload();
 
-//判断是否开启Debug
-if((defined('YEPF_IS_DEBUG') && YEPF_IS_DEBUG) || (isset($_REQUEST['debug']) && strpos($_REQUEST['debug'], YEPF_DEBUG_PASS) !== false))
-{
-	//Debug模式将错误打开
-	ini_set('display_errors', true);
-	//设置错误级别
-	error_reporting(YEPF_ERROR_LEVEL);
-	
-	//打开 FireBug 支持 （注意：仅FirePHP标志存在的情况下）
-	if(preg_match('/FirePHP/',$_SERVER['HTTP_USER_AGENT'])){
-		//开启ob函数
-		ob_start();
-		//Debug开关
-		\yoka\Debug::start();
-		//注册shutdown函数用来Debug显示 : 手工处理显示的场景，请设置： define('MANUAL_DEBUG_SHOW',true);
-		if(!defined('MANUAL_DEBUG_SHOW') || MANUAL_DEBUG_SHOW != true) register_shutdown_function(array('\yoka\Debug', 'show'));
+//打开 FireBug 支持 （注意：仅FirePHP标志存在的情况下）
+if(preg_match('/FirePHP/',$_SERVER['HTTP_USER_AGENT'])){
+	//判断是否开启error_report
+	if((defined('YEPF_IS_DEBUG') && YEPF_IS_DEBUG) || (isset($_REQUEST['debug']) && strpos($_REQUEST['debug'], YEPF_DEBUG_PASS) !== false)){
+		//Debug模式将错误打开
+		ini_set('display_errors', true);
+		//设置错误级别
+		error_reporting(YEPF_ERROR_LEVEL);
+	}else{
+		//Debug模式将错误打开
+		ini_set('display_errors', false);
 	}
+
+	//开启ob函数
+	ob_start();
+	//Debug开关
+	\yoka\Debug::start();
+	//注册shutdown函数用来Debug显示 : 手工处理显示的场景，请设置： define('MANUAL_DEBUG_SHOW',true);
+	if(!defined('MANUAL_DEBUG_SHOW') || MANUAL_DEBUG_SHOW != true) register_shutdown_function(array('\yoka\Debug', 'show'));
 }
 //读取系统配置文件
 if(!defined('ENV_PATH'))define('ENV_PATH','/WORK/CONF');
