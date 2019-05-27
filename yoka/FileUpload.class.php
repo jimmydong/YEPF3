385503f31c459d301763a83717b4a1d9dd6483aa
jimmy.dong <jimmy.dong@gmail.com>
2019-05-27 10:46:15 +0800

    fix bug

--- yoka/FileUpload.class.php
+++ yoka/FileUpload.class.php
@@ -164,7 +164,7 @@ class FileUpload{
 	public static function uploadFile($name, $full = false){
 		if(! $file_path	= self::create($_FILES[$name]['name'], $_FILES[$name]['tmp_name'])) return \yoka\YsError::error('上传失败：无上传文件');
 		if(! $real_path = self::getRealPath($file_path_name)) return \yoka\YsError::error('上传失败：文件大小异常？');
-		if(! in_array(pathinfo($real_path, PATHINFO_EXTENSION), self::$file_ext_allowed)) return \yoka\YsError::error('上传失败：文件类型错误');
+		if(! self::$file_ext_allowed[pathinfo($real_path, PATHINFO_EXTENSION)]) return \yoka\YsError::error('上传失败：文件类型错误');
 		
 		if($full) return ['file_path'=>$file_path, 'real_path'=>$real_path, 'url'=>self::getUrl($file_path)];
 		else return $file_path;
