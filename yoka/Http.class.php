fbb922d4f4d4f6cfc85cb9f34a87c5ae751e5a78
jimmy <jimmy.dong@gmail.com>
2021-05-27 12:39:28 +0800

    update

--- yoka/Http.class.php
+++ yoka/Http.class.php
@@ -265,7 +265,7 @@ class Http{
 		if($header){
 			$h = array();
 			foreach($header as $k=>$v){
-				$h[] = $k . ":" . $v; //注意：不做urlencode
+				$h[$k] = $v; //注意：不做urlencode
 			}
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
 		}
@@ -338,7 +338,7 @@ class Http{
 		if($header){
 			$h = array();
 			foreach($header as $k=>$v){
-				$h[] = $k . ":" . $v; //注意：不做urlencode
+				$h[$k] = $v; //注意：不做urlencode
 			}
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
 		}
@@ -427,7 +427,7 @@ class Http{
 		if($header){
 			$h = array();
 			foreach($header as $k=>$v){
-				$h[]= urlencode($k) .":" . urlencode($v);
+				$h[$k] = $v;
 			}
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
 		}
@@ -503,7 +503,7 @@ class Http{
 		if($header){
 			$h = array();
 			foreach($header as $k=>$v){
-				$h[]= urlencode($k) .":" . urlencode($v);
+				$h[$k] = $v;
 			}
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
 		}
@@ -583,7 +583,7 @@ class Http{
 		if($header){
 			$h = array();
 			foreach($header as $k=>$v){
-				$h[]= urlencode($k) .":" . urlencode($v);
+				$h[$k] = $v;
 			}
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
 		}
@@ -678,7 +678,7 @@ class Http{
 		$h = ['Content-Type'=>'application/json'];
 		if($header){
 			foreach($header as $k=>$v){
-				$h[]= $k .":" . $v;  //注意： 不做urlencode
+				$h[$k] = $v;  //注意： 不做urlencode
 			}
 		}
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
